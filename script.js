95994d7d1100658bfa88212945e01dba69ec4042
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-29 09:55:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+
+// Check if cart data exists in session storage and retrieve it
+let cartData = sessionStorage.getItem("cartData");
+if (cartData) {
+  cartData = JSON.parse(cartData);
+} else {
+  cartData = [];
+}
 
 // Render product list
 function renderProducts() {
@@ -22,16 +32,65 @@ function renderProducts() {
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	 // Clear existing cart list
+  cartList.innerHTML = "";
+
+  // Iterate over the cart items and create list items
+  cartData.forEach((item) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${item.name} - $${item.price} <button class="remove-from-cart-btn" data-id="${item.id}">Remove from Cart</button>`;
+    cartList.appendChild(li);
+  });
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	const product = products.find((p) => p.id === productId);
+  if (product) {
+    cartData.push(product);
+    // Update session storage with the updated cart data
+    sessionStorage.setItem("cartData", JSON.stringify(cartData));
+    renderCart();
+  }
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+	 const index = cartData.findIndex((item) => item.id === productId);
+  if (index !== -1) {
+    cartData.splice(index, 1);
+    // Update session storage with the updated cart data
+    sessionStorage.setItem("cartData", JSON.stringify(cartData));
+    renderCart();
+  }
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+	cartData = [];
+  // Remove cart data from session storage
+  sessionStorage.removeItem("cartData");
+  renderCart();
+}
+// Add event listener to product list for add-to-cart buttons
+productList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("add-to-cart-btn")) {
+    const productId = parseInt(event.target.dataset.id);
+    addToCart(productId);
+  }
+});
+
+// Add event listener to cart list for remove-from-cart buttons
+cartList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const productId = parseInt(event.target.dataset.id);
+    removeFromCart(productId);
+  }
+});
+
+// Add event listener to the clear cart button
+clearCartBtn.addEventListener("click", clearCart);
 
 // Initial render
 renderProducts();
